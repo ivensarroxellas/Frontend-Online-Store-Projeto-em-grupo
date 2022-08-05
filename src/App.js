0b32f4a664c94d95223d47ff5abf35897a1b2ad8
Ivens <ivensarroxellas@hotmail.com>
2022-08-05 15:50:23 -0300

    req 2- input de pesquisa e correção de function para class

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+import MainScreen from './pages/MainScreen';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    nameEntered: '',
+  };
+
+  handleChange = (event) => {
+    this.setState(() => ({ [event.target.name]: event.target.value }),
+      this.handledButton);
+  };
+
+  render() {
+    const { nameEntered } = this.state;
+    return (
+      <>
+        <p>olá</p>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={ () => (<MainScreen
+              nameEntered={ nameEntered }
+              onInputChange={ this.handleChange }
+            />) }
+          />
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
