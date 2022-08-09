f5c9b1543fcbe294ae533941a1cc43e5112e9cb4
ivensarroxellas <104789630+ivensarroxellas@users.noreply.github.com>
2022-08-09 15:05:25 -0300

    Update MainScreen.js

--- src/pages/MainScreen.js
+++ src/pages/MainScreen.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { Redirect, Link } from 'react-router-dom';
-import CardList from '../Components/CardList';
-import Card from '../Components/Card';
 import PropTypes from 'prop-types';
+import CardList from '../Components/CardList';
 import Category from '../Components/Category';
 import * as api from '../services/api';
 
